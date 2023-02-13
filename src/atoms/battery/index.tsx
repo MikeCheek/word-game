c53c54dd3896ef170aca9a2e22f2256740f1aa27
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2023-02-13 17:30:07 +0100

    Fix battery safari issue

--- src/atoms/battery/index.tsx
+++ src/atoms/battery/index.tsx
@@ -1,14 +1,14 @@
-import React, {useEffect, useState} from 'react'
-import * as styles from './index.module.scss'
-import Lightning from '../../assets/lightning.svg'
+import React, { useEffect, useState } from 'react';
+import * as styles from './index.module.scss';
+import Lightning from '../../assets/lightning.svg';
 
 const Index = () => {
   const [battery, setBattery] = useState<{
-    level: number
-    charging: boolean
-    chargingTime: number
-    discargingTime: number
-  }>({level: -1, charging: false, chargingTime: -1, discargingTime: -1})
+    level: number;
+    charging: boolean;
+    chargingTime: number;
+    discargingTime: number;
+  }>({ level: -1, charging: false, chargingTime: -1, discargingTime: -1 });
 
   const updateBattery = (battery: any) => {
     setBattery({
@@ -16,37 +16,38 @@ const Index = () => {
       charging: battery.charging,
       chargingTime: battery.chargingTime,
       discargingTime: battery.discargingTime,
-    })
-  }
+    });
+  };
 
   const getBattery = () => {
-    if (window.navigator)
+    //@ts-ignore
+    if (window.navigator && window.navigator.getBattery)
       //@ts-ignore
       window.navigator.getBattery().then((battery) => {
-        updateBattery(battery)
+        updateBattery(battery);
         battery.addEventListener('levelchange', () => {
-          updateBattery(battery)
-        })
+          updateBattery(battery);
+        });
         battery.addEventListener('chargingchange', () => {
-          updateBattery(battery)
-        })
+          updateBattery(battery);
+        });
         battery.addEventListener('chargingtimechange', () => {
-          updateBattery(battery)
-        })
+          updateBattery(battery);
+        });
         battery.addEventListener('dischargingtimechange', () => {
-          updateBattery(battery)
-        })
-      })
-  }
+          updateBattery(battery);
+        });
+      });
+  };
 
   useEffect(() => {
-    getBattery()
-  }, [])
+    getBattery();
+  }, []);
 
   return battery.level >= 0 ? (
     <>
-      <div className={styles.battery} style={{opacity: battery.charging ? 0.8 : 0.7}}>
-        <div className={styles.level} style={{width: battery.level * 100 + '%'}}></div>
+      <div className={styles.battery} style={{ opacity: battery.charging ? 0.8 : 0.7 }}>
+        <div className={styles.level} style={{ width: battery.level * 100 + '%' }}></div>
         <span>
           {battery.level * 100}% {battery.charging ? <Lightning fill="var(--orange)" /> : null}
         </span>
@@ -61,7 +62,7 @@ const Index = () => {
     </>
   ) : (
     <></>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
